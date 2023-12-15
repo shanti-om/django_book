934263af231ce936de4cb5dbd2634b995835dccc
shanti-om <shanti-om@mail.ru>
2023-12-15 19:31:58 +0300

    css and media work fine

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -6,9 +6,10 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    slug = models.CharField(max_length=200)
+    body = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+    published = models.DateTimeField(blank=True, null=True)
     status = models.BooleanField()
 
     def publish(self):
