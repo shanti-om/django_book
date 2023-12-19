8ac5a73cb4c9c293540fc24b7b40efaba995c14f
shanti-om <shanti-om@mail.ru>
2023-12-19 15:00:39 +0300

    new design

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,19 +1,41 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Черновик'
+        PUBLISHED = 'PB', 'Опубликовано'
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     title = models.CharField(max_length=200)
     slug = models.CharField(max_length=200)
     body = models.TextField()
     created = models.DateTimeField(default=timezone.now)
-    published = models.DateTimeField(blank=True, null=True)
-    status = models.BooleanField()
+    publish = models.DateTimeField(blank=True, null=True)
+    status = models.CharField(max_length=2, choices=Status.choices,
+                              default=Status.DRAFT)
+    objects = models.Manager()  # менеджер, применяемый по умолчанию
+    published = PublishedManager()  # конкретно-прикладной менеджер
 
-    def publish(self):
-        self.published_date = timezone.now()
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']), ]
+
+    def get_description(self):
+        return self.body[:200] + ' . . .'
+
+    def pub(self):
+        self.published = timezone.now()
         self.save()
 
     def __str__(self):
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -10,7 +10,8 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all()
+    # posts = Post.objects.all()
+    posts = Post.published.all()
     # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
     return render(request, 'blog/post_list.html', {'posts': posts})
