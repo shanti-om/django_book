a93096d754183bb29e5c5dd0176c095217beb8c6
shanti-om <shanti-om@mail.ru>
2023-12-20 18:32:01 +0300

    image added into post

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
-    path('', include('blog.urls', namespace='blog')),
 ]
